4931785abacb5977217525976be28ae988afc515
Wendel Harrison <wh68639@gmail.com>
2022-08-12 13:20:17 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+const restartGame = document.querySelector('btn-restart')
+
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        
+        mario.classList.remove('jump');
+        
+    }, 500);
+}
+
+const loop = setInterval(() => {
+    
+    const pipePosition = pipe
+.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+
+    console.log(marioPosition)
+
+if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+    playAgain()
+
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = './images/go.png'
+    mario.style.width = '75px'
+    mario.style.marginLeft = '50px'
+
+    clearInterval(loop);
+
+}
+}, 10);
+
+function playAgain() {
+    document.querySelector('.btn-restart').style.display = 'flex'
+}
+
+function restart() {
+    document.location.reload(true)
+
+}
+
+
+document.addEventListener('keydown', jump)
